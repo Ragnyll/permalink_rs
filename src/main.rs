a835215d0ac9b434b05d2f667a5d182f159f9b2e
ragnyll <ragnyll@gallowzhumour.dev>
2021-10-09 00:41:45 -0500

    once i figure out this relative path junk im good

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,8 @@ extern crate exitcode;
 
 use clap::{App, Arg};
 
+use pathdiff;
+
 use std::collections::HashMap;
 use std::env;
 use std::path::Path;
@@ -13,13 +15,10 @@ use std::process::exit;
 const PATH_PARAM: &str = "PATH";
 const LINE_RANGE_PARAM: &str = "LINE_RANGE";
 
-const GITHUB_BASE_URL: &str = "heck";
-const GITLAB_BASE_URL: &str = "better";
-
 fn main() {
-    let mut git_base_urls: HashMap<String, String> = HashMap::new();
-    git_base_urls.insert(String::from("git@gitlab.com"), String::from(GITLAB_BASE_URL));
-    git_base_urls.insert(String::from("git@github.com"), String::from(GITLAB_BASE_URL));
+    let mut git_commit_link_scheme: HashMap<String, String> = HashMap::new();
+    git_commit_link_scheme.insert(String::from("git@gitlab.com"), String::from("https://gitlab.com/REPO-NAME/-/blob/COMMIT/FILE#LINE_RANGE"));
+    git_commit_link_scheme.insert(String::from("git@github.com"), String::from("https://github.com/REPO/blob/COMMIT/FILE#LINE_RANGE"));
 
     let matches = App::new("git-permalink-rs")
         .version("0.1")
@@ -52,29 +51,67 @@ fn main() {
         exit(exitcode::USAGE)
     }
 
-    let base_url;
+    let mut url_pattern = "";
     let stdout = String::from_utf8(output.stdout.to_vec()).expect("Unable to parse stdout as valid Utf8");
     let stdout = stdout.trim();
 
-    for key in git_base_urls.keys() {
+    for key in git_commit_link_scheme.keys() {
         if stdout.contains(key) {
-            base_url = git_base_urls.get(key).unwrap();
+            url_pattern = git_commit_link_scheme.get(key).unwrap();
             break;
         }
     }
 
-    // TODO: parse out the repo name
-    let repo_name = "getreponame"
+    let repo_name = stdout.split(":").collect::<Vec::<&str>>()[1];
+    let repo_name = String::from(repo_name).replace(".git", "");
+
+    let output = Command::new("git")
+        .arg("rev-parse")
+        .arg("HEAD")
+        .output()
+        .unwrap();
+    if output.status.code() != Some(0) {
+        eprintln!("Unable to run git commands on {}", path.to_str().unwrap());
+        exit(exitcode::USAGE)
+    }
+    let current_commit = String::from_utf8(output.stdout.to_vec()).expect("Unable to parse stdout as valid Utf8");
+    let current_commit = current_commit.trim();
 
     let mut lines: Vec<&str> = matches.value_of(LINE_RANGE_PARAM).unwrap().split("-").collect();
     lines.retain(|&e| !e.is_empty());
 
-    match lines.len() {
-        1 => (),
-        2 => (),
+    let line_range = match lines.len() {
+        1 => {
+            format!("L{}", lines[0])
+        },
+        2 => {
+            format!("L{}-L{}", lines[0], lines[1])
+        },
         _ => {
             eprintln!("LINE_RANGE must be in the format of startLine[-endLine]");
             exit(exitcode::USAGE);
         },
     };
+
+    let output = Command::new("git")
+        .arg("rev-parse")
+        .arg("--show-toplevel")
+        .output()
+        .unwrap();
+    if output.status.code() != Some(0) {
+        eprintln!("Unable to run git commands on {}", path.to_str().unwrap());
+        exit(exitcode::USAGE)
+    }
+    let git_repo_base_path = String::from_utf8(output.stdout.to_vec()).expect("Unable to parse stdout as valid Utf8");
+
+    let relative_path_to_file = pathdiff::diff_paths(Path::new(&git_repo_base_path), Path::new(path));
+    println!("{}", path.to_str().unwrap());
+    println!("{:?}", relative_path_to_file);
+    println!("{}", relative_path_to_file.unwrap().into_os_string().to_str().unwrap());
+    let file_path = "HECK";
+
+
+    let final_link = url_pattern.replace("REPO-NAME", &repo_name).replace("COMMIT", &current_commit).replace("FILE", file_path).replace("LINE_RANGE", &line_range);
+
+    println!("{}", final_link);
 }
